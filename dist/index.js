7234b55b3d0200f814a78b851f79e55b4ef76d18
Youhana Naseim <youhana.naseim@gmail.com>
2025-07-03 12:44:52 -0400

    Add more logging for errors.

--- dist/index.js
+++ dist/index.js
@@ -33580,8 +33580,7 @@ async function run() {
             }
         });
         if (result.status < 200 || result.status >= 300) {
-            const msg = `Failed to submit request, recieved a non-2XX response code : ${result.status}`;
-            core.error(JSON.stringify(result));
+            const msg = `Failed to submit request, recieved a non-2XX response code : ${result.status} : ${JSON.stringify(result)}`;
             core.error(msg);
             core.setFailed(msg);
             return;
